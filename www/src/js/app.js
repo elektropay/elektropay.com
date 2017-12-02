6a092d7809621621ea9bde6ac72c32428df91c9d
Elektropay <richie@elektropay.com>
2017-12-02 11:06:05 -0500

    Update app.js

--- www/src/js/app.js
+++ www/src/js/app.js
@@ -1,3 +1,29 @@
+/* -- jpreloader
+   -- src: http://www.inwebson.com/jquery/jpreloader-a-preloading-screen-to-preload-images/
+   ---------------------------- */
+
+$(document).ready( function() {
+
+    //calling jPreLoader
+    $('body').jpreLoader({
+      splashID: "#preloader",
+      loaderVPos: '60%',
+      autoClose: true
+    });
+
+ });
+
+/* -- NiceScroll
+   -- src: www.areaaperta.com/nicescroll
+   ---------------------------- */
+
+$(document).ready(
+  function() {  
+    $("html").niceScroll({cursorcolor:"#2C2F35"});
+  }
+);
+
+
 // Sticky Nav Functionality in Vanilla JS
 
 var header = $("#header");
